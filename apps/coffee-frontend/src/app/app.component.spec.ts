a7c3db5f04e041185a82b7ce375d251704455c0b
sanjeev <s.ramappa@cloudwise.nl>
2024-11-15 06:45:31 +0100

    main: removed nx component

--- apps/coffee-frontend/src/app/app.component.spec.ts
+++ apps/coffee-frontend/src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents();
   });
 
